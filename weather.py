f234632793e3cbf2de2e99105cabb31da8d22c9e
torlye <tor_lye@hotmail.com>
2020-11-02 20:07:09 +0100

    Remove console messages when reloading weather

--- weather.py
+++ weather.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import subprocess
+import os
+import signal
 
 interval = 10*60
 location = 'Norway/Oslo/Oslo/Oslo'
@@ -7,8 +9,10 @@ location = 'Norway/Oslo/Oslo/Oslo'
 def showMeteogram():
 	try:
 		wgetProc = subprocess.run(['wget', '-NqO', '-', 'https://www.yr.no/place/'+location+'/meteogram.png'], stdout=subprocess.PIPE)
-		subprocess.run(['fim', '-aqi', '--no-history'], input=wgetProc.stdout, timeout=interval)
+		p = subprocess.Popen(['fim', '-aqi', '--no-history'], stdin=subprocess.PIPE, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+		p.communicate(wgetProc.stdout, timeout=interval)
 	except subprocess.TimeoutExpired:
+		p.send_signal(signal.SIGINT)
 		pass
 
 print('Showing meteogram for '+location+', refreshing every '+str(interval)+' seconds')
