2410c4516e9e559d79a69cd05600390f788deafa
torlye <tor_lye@hotmail.com>
2020-11-04 16:15:36 +0100

    Menu fixes

--- menu.py
+++ menu.py
@@ -7,40 +7,32 @@ import select
 import signal
 import RPi.GPIO as GPIO
 
-BUTTON_GPIO = 16
+BUTTON_GPIO = 10
 countdown = 20
 selection = 0
 menuitems = ['Photos', 'Videos', 'Weather']
-scripts = ['photo.py', 'video.py', 'weather.py']
+scripts = ['photo.py', 'video.py', 'weather.py']
 
 def printMenu():
-    os.system('clear')
-    for x in range(3):
-        print(('\u25BA' if x == selection else ' ') + ' ' + menuitems[x])
-    print('')
-    print('Selection option will start in: '+str(countdown))
-
-def signal_handler(sig, frame):
-    GPIO.cleanup()
-    sys.exit(0)
+	os.system('clear')
+	for x in range(3):
+		print(('\u25BA' if x == selection else ' ') + ' ' + menuitems[x])
+	print('')
+	print('Selection option will start in: '+str(countdown))
 
 def button_pressed_callback(channel):
-    selection = (selection + 1) % len(menuitems)
+	global selection
+	selection = (selection + 1) % len(menuitems)
+	printMenu()
 
-def timeout_input(prompt, timeout=1, default=""):
-    inputs, outputs, errors = select.select([sys.stdin], [], [], timeout)
-    return (0, sys.stdin.readline().strip()) if inputs else (-1, default)
-
-GPIO.setmode(GPIO.BCM)    
-GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(BUTTON_GPIO, GPIO.FALLING, callback=button_pressed_callback, bouncetime=200)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(BUTTON_GPIO, GPIO.RISING, callback=button_pressed_callback, bouncetime=200)
 
 while countdown > 0:
-    printMenu()
-    time.sleep()
-    #inp, text = timeout_input("")
-    #if (inp == 0):
-    #    button_pressed_callback()
-    countdown -= 1
+	printMenu()
+	time.sleep(1)
+	countdown -= 1
 
-subprocess.run(['python3', script[selection]])
+GPIO.cleanup()
+subprocess.run(['python3', scripts[selection]])
