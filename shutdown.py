7c7b226844d7be2f5d0165e65fb60bf1ee114b8d
torlye <foo@bar.com>
2020-11-05 19:01:42 +0100

    Fix shutdown

--- shutdown.py
+++ shutdown.py
@@ -8,17 +8,16 @@ conf = config['shutdown']
 BUTTON_GPIO = conf.getint('GPIO_button_pin')
 time1 = sys.maxsize
 
-def button_pressed_in(channel):
+def button_pressed(channel):
 	global time1
-	time1 = time.time()
-
-def button_pressed_out(channel):
-	if time.time() - time1 > 5:
-		GPIO.cleanup()
-		os.system("sudo shutdown -h now")
-		quit()
+	if GPIO.input(BUTTON_GPIO): # rising edge
+		time1 = time.time()
+	else # falling edge
+		if time.time() - time1 > 5: # button was held for 5s
+			GPIO.cleanup()
+			os.system("sudo shutdown -h now")
+			quit()
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(BUTTON_GPIO, GPIO.RISING, callback=button_pressed_in, bouncetime=200)
-GPIO.add_event_detect(BUTTON_GPIO, GPIO.FALLING, callback=button_pressed_out, bouncetime=200)
+GPIO.add_event_detect(BUTTON_GPIO, GPIO.BOTH, callback=button_pressed, bouncetime=200)
